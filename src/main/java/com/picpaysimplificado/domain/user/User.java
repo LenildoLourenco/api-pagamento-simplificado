42326d91da099fb1966ace1299097ed94a734318
LenildoLourençodeSou <lourencolenildo@gmail.com>
2024-05-27 20:28:14 -0300

    feat: create services user and transaction

--- src/main/java/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,10 +1,11 @@
 package com.picpaysimplificado.domain.user;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+//import lombok.AllArgsConstructor;
+//import lombok.EqualsAndHashCode;
+//import lombok.Getter;
+//import lombok.Setter;
 
 import java.math.BigDecimal;
 
